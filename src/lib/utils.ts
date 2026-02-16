5de27ef529e0d1639864de141d22567c84c9ba4e
Jad Halawi <jadhalawi@Jads-MacBook-Pro.local>
2026-02-16 17:52:02 +0100

    revert: util documentation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-// cn util
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
