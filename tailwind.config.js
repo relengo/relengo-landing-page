147e00df62066e5e4eccc82fe0eb32ac2cb2abf2
Zafer <zafer.qadi@gmail.com>
2026-02-16 19:30:58 +0100

    cleanups

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: { extend: {} },
   plugins: [],
 };
