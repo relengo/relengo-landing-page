c9430acc65942b9351c619abbb0e0554b35866ea
Jad Halawi <jadhalawi@Jads-MacBook-Pro.local>
2026-02-27 10:00:22 +0100

    public redirect and middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
- 
+
 export default createMiddleware({
-  // A list of all locales that are supported in this application
   locales: ['en', 'de'],
- 
-  // Used when no locale matches
   defaultLocale: 'en',
-  localeDetection: false
+  localeDetection: false,
 });
- 
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+  ]
 };
